4e70d939656c12ee1ddcc4dbd9a0dca7b7f50566
mslacker <artem.kunashev@gmail.com>
2020-08-06 16:26:45 +0400

    Добавил раздачу upload.

--- web/web/urls.py
+++ web/web/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
@@ -23,4 +24,4 @@ admin.autodiscover()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('test_storage.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # TODO Add nginx for production
