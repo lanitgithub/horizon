1a2293270f43caeac280e3f3162ec5b4d0c8d169
mslacker <artem.kunashev@gmail.com>
2020-08-07 18:45:38 +0400

    Начал добавлять celery + redis.

---
+++ web/web/__init__.py
@@ -0,0 +1,4 @@
+from .celery_app import celery_app
+
+
+__all__ = ('celery_app',)
--- web/web/settings.py
+++ web/web/settings.py
@@ -133,3 +133,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 FIRST_DAY_OF_WEEK = 1
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
