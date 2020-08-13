677f4b99de4e19acdba1411c4b71c2731a98373e
mslacker <artem.kunashev@gmail.com>
2020-08-13 16:48:41 +0400

    Первый рабочий вариант (с опцией --pool=solo) на основе офф документации и https://stackoverflow.com/questions/27357732/celery-task-always-pending?rq=1

--- web/test_storage/tasks.py
+++ web/test_storage/tasks.py
@@ -1,6 +1,32 @@
+# Create your tasks here
+from __future__ import absolute_import, unicode_literals
+
 from celery import shared_task
+from .models import Project
 
 
 @shared_task
-def adding_task(x, y):
+def add(x, y):
     return x + y
+
+
+@shared_task
+def mul(x, y):
+    return x * y
+
+
+@shared_task
+def xsum(numbers):
+    return sum(numbers)
+
+
+@shared_task
+def count_projects():
+    return Project.objects.count()
+
+
+@shared_task
+def rename_Project(project_id, name):
+    w = Project.objects.get(id=project_id)
+    w.name = name
+    w.save()
--- web/web/settings.py
+++ web/web/settings.py
@@ -162,7 +162,6 @@ LOGGING = {
 }
 
 
-
 CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
