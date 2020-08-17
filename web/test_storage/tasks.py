6a969eed28b16a413c5bb85a5cc11873127b07ff
mslacker <artem.kunashev@gmail.com>
2020-08-17 17:55:36 +0400

    Скачивание гитлаб перенес в celery task.

--- web/test_storage/tasks.py
+++ web/test_storage/tasks.py
@@ -1,5 +1,8 @@
-# Create your tasks here
 from __future__ import absolute_import, unicode_literals
+
+import os
+import re
+import tempfile
 
 import gitlab
 from celery import shared_task
@@ -35,14 +38,43 @@ def rename_Project(project_id, name):
     w.save()
 
 
-def _get_jmeter_source(test_id, name):
+def _get_jmeter_source(test_id):
     """
     Скачать из гитлаба исходники (и данные) для запуска теста.
     """
     test = Test.objects.get(id=test_id)
     jmeter_source = test.testplan.jmeter_source
-    gl = gitlab.Gitlab('https://gitlab.dks.lanit.ru', private_token=jmeter_source.token)
-    gitlab.v4.objects.ProjectFileManager(gl, ref='master')
+    url = jmeter_source.url
+
+    regex = r'^(?P<main_url>https+:\/\/.+?)\/(?P<group_name>.+?)\/(?P<project_name>.+?)\/-\/blob\/' \
+            r'(?P<branch_name>.+?)\/(?P<path>.+\.jmx)$'
+
+    match = re.match(regex, url)
+
+    if not match:
+        pass  # TODO raise exception
+
+    url_components = match.groupdict()
+    print(url_components)
+
+    gl = gitlab.Gitlab(url_components['main_url'], private_token=jmeter_source.token)
+
+    # get project name by id
+    project_id = gl.search('projects', url_components['project_name'])[0]['id']
+
+    project = gl.projects.get(project_id)
+
+    temp_dir = tempfile.TemporaryDirectory(prefix='jmeter_sources_')
+    print(temp_dir.name)
+
+    base_script_filename = os.path.basename(url_components['path'])
+
+    with open(os.path.join(temp_dir.name, base_script_filename), 'wb') as f:
+        project.files.raw(file_path=url_components['path'], ref=url_components['branch_name'], action=f.write)
+
+
+    # call this after test stopped
+    #temp_dir.cleanup()
 
 
 
